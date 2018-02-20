7a1fc8982b69c796bc2b4f0de3df5c289e12b125
COLLECTOR\henrik.forsberg <henrik.forsberg@collectorbank.se>
2018-02-20 16:58:43 +0100

    tesdt

--- src/postTweet.R
+++ src/postTweet.R
@@ -3,10 +3,12 @@
 if (!require('twitteR')) install.packages("twitteR")
 
 # Set API Keys ---------------------------------------------------
-api_key <- "aLr5A0XD77RLre9teUGVkE6pW"
-api_secret <- "OM9A34Jf7GAGgmClNSdHiykfM5NnlJAPhWiCpKRjsFLH4GIxB4"
-access_token <- "555298661-KisfodWZqSldn9ilnczGnjCv8bzg8SnKWkMpRXFu"
-access_token_secret <- "Lj4MQe6MrGBxZGI1OTgG81EOZCp3JaOnXZdUlAiUFV2gH"
+credentials <- fromJSON(file=credentials_file)
+
+api_key <- credentials$twitter$consumer_key
+api_secret <- credentials$twitter$consumer_secret
+access_token <- credentials$twitter$access_token
+access_token_secret <- credentials$twitter$access_secret
 
 # Posting to Twitter ---------------------------------------------------
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
