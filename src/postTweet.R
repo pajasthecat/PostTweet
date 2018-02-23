b818663dcc29d4bcd0406649d6d25388775b3c47
Henrik Forsberg <henrik.forsberg1990@gmail.com>
2018-02-23 15:32:16 +0100

    Update postTweet.R

--- src/postTweet.R
+++ src/postTweet.R
@@ -1,13 +1,18 @@
 options(repos = "https://mran.microsoft.com")
 # Installing packages ------------------------------------------------------------------
 
-if (!require('twitteR')) install.packages("twitteR")
+if (!require('httr')) install.packages('httr')
 if (!require('jsonlite')) install.packages('jsonlite')
+if (!require('ggplot2')) install.packages('ggplot2')
+if (!require('twitteR')) install.packages("twitteR")
+if (!require('rsvg')) install.packages("rsvg")
 if (!require('rjson')) install.packages("rjson")
-
-library('twitteR')
 library('rjson')
+library('rsvg')
+library('twitteR')
+library('httr')
 library('jsonlite')
+library('ggplot2')te')
 
 # Set API Keys ---------------------------------------------------
 credentials_file = "credentials.json"
